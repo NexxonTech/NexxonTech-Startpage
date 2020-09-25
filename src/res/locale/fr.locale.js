3ad75a221c3e952545a9521a6e47535bbc0b8ee1
lentidas <33546359+lentidas@users.noreply.github.com>
2020-09-25 14:21:02 +0200

    Added french translation

---
+++ src/res/locale/fr.locale.js
@@ -0,0 +1,57 @@
+export default {
+	searchWith: {
+		duckduckgo: "Rechercher avec DuckDuckGo",
+		google: "Rechercher avec Google",
+		wolfram: "Demander à Wolfram|Alpha",
+		url: "Saisissez une URL",
+	},
+	footer: {
+		photoBy: "Photo par",
+		on: "sur",
+	},
+	topRightMenu: {
+		manageBookmarks: "Gérer les favoris",
+		settings: "Réglages",
+	},
+	bookmarksManagement: {
+		title: "Gérer les favoris",
+		new: "Nouveau",
+		bookmarkTitle: "Titre du favori",
+		url: "URL du favori",
+		editDisabled: "Modification verrouillée par l'hôte de cette page",
+		save: "Enregistrer",
+	},
+	settings: {
+		title: "Réglages",
+		searchEngine: "Moteur de recherche",
+		languages: "Langue",
+		twelveHours: "Format de l'horloge",
+		secondsInClock: "Horlogue avec secondes",
+		dateFormat: "Format de date",
+		startpageTitle: "Titre de la page",
+		recommended: "Recommandé",
+		save: "Enregistrer",
+	},
+	quickNotes: {
+		title: "Notes rapides",
+		save: "Enregistrer",
+	},
+	toasts: {
+		welcome: {
+			title: "Bienvenu!",
+			paragraphs: [
+				"Bienvenue dans la nouvelle version de NexxonTech Startpage!",
+				"L'application a été mise à jour pour être encore plus rapide et élégante, parfaite pour n'importe quel PC.",
+				"Essayez-la maintenant pour découvrir toutes les nouvelles fonctionnalités et la personnaliser selon vos préférences.",
+			],
+		},
+		cookie: {
+			title: "Avis sur les cookies",
+			paragraphs: [
+				"Merci de votre intérêt pour NexxonTech Startpage.",
+				"Nous vous montrons ce message pour vous rappeler que cette application utilise des cookies (API localStorage, pour être précis) afin de stocker les informations nécessaires au fonctionnement du software lui-même.",
+				"Aucune intervention de votre part n'est nécessaire.",
+			],
+		},
+	},
+};
