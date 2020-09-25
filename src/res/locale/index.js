0fbcda35d844ec72b29a8ab2ab71801cc9a9c351
Riccardo Sacchetto <rsacchetto@nexxontech.it>
2020-09-25 21:12:23 +0200

    Completed the integration of the French language

--- src/res/locale/index.js
+++ src/res/locale/index.js
@@ -1,9 +1,11 @@
 import it from "./it.locale";
 import en from "./en.locale";
-import pt from "./pt.locale"
+import pt from "./pt.locale";
+import fr from "./fr.locale";
 
 export default {
 	it,
 	en,
-	pt
+	pt,
+	fr,
 };
